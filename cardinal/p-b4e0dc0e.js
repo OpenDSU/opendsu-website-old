9a750f2c643e6b9d6c4fef7d42b100306143e5bd
Paul Isache <paul@isache.com>
2020-09-29 21:22:41 +0300

    Add missing rfcs as described

---
+++ cardinal/p-b4e0dc0e.js
@@ -0,0 +1,1 @@
+import{E as s}from"./p-d9d3000e.js";const t=s.PSK_BUTTON_EVT;class o extends CustomEvent{constructor(s,o,r){super(s,r),this.getEventType=function(){return t},this.data=o}}export{o as P}
