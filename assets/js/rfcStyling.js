6044867e5bade27e71599d968b38cdbce82ee03b
aTravelingN3rd <valentin.gerard.atn@gmail.com>
2020-12-04 17:36:03 +0100

    rfcStyling

---
+++ assets/js/rfcStyling.js
@@ -0,0 +1,33 @@
+
+
+function changeCss(element){
+
+
+
+		var url = element.getAttribute("src");
+        var status;
+            
+        // create div and replace iframe
+        let d = document.createElement('div');
+        d.classList.add("embedded-doc"); // optional
+        d.style.width = "100%";
+        element.parentElement.replaceChild(d, element);
+
+        // CORS request
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onload = function() {
+                    // display response
+            splitResponse=xhr.responseText.split('<link');
+            split2=splitResponse[1].split('.ico">')[1];
+            responseWithCss=splitResponse[0]+'<link rel="stylesheet" href="/assets/css/rfcStyling.css">'+split2;
+            tmpStatus=responseWithCss.split('&lt;status&gt;');
+            status=tmpStatus[1];
+            cleanResponse=tmpStatus[0]+tmpStatus[2];
+            cleanResponse=cleanResponse.split('<div id="interval">')[0]+'<div id="interval"><p> Status: <span class="'+status+'">'+status+'</span></p></div></div><div id="contents">'+cleanResponse.split('<div id="contents">')[1];
+            d.innerHTML = cleanResponse;
+        };
+        xhr.send();
+       
+
+	}
